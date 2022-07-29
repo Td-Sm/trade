65e32dae2b7e63bc49bd304ac8cbcb14cae8aee5
Td-Sm <1225742802@qq.com>
2022-07-29 13:33:28 +0800

    数组一类
    初见

---
+++ c语言初学/c语言初学/数组.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	//printf("c;\test\32\test.c\n");
+	//printf("%d\n", strlen("c;\test\32\test.c"));//\32是一个转义字符。——32是两个八进制数字
+	//32作为8进制代表的那个十进制数字作为ASCII码值，对应的字符
+	//32-->10进制 26-->作为ASCII码值代表的字符。
+	//
+	//char arr1[] = "abc";
+	//char arr2[] = { 'a','b','c'};
+	////printf("%s\n%s\n", arr1, arr2);
+	//printf("%d\n%d\n", strlen(arr1), strlen(arr2));
+	return 0;
+}
