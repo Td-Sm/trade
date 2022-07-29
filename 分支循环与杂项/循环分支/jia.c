3d5950bc53b391e25b1be8284a901ba90dcbac32
Td-Sm <1225742802@qq.com>
2022-07-30 05:04:49 +0800

    分支循环
    里面还有数组一类的杂项……传了

---
+++ 分支循环与杂项/循环分支/jia.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int Add(int a, int b)
+//{
+//	int sum = a + b;
+//	return sum;
+//}
+//int main()
+//{
+//	int n1, n2, lingo;
+//	printf("田所浩二有几个林檎？\n");
+//	scanf("%d", &n1);
+//	printf("你有几个林檎？\n");
+//	scanf("%d", &n2);
+//	lingo = Add(n1, n2);
+//	printf("太好了！你们一共有%d个林檎能用，快去开银趴吧！", lingo);
+//	return 0;
+//}
---
+++ 分支循环与杂项/循环分支/源.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int line = 0;
+//		printf("来和田所浩二一起玩撅林檎游戏吧\n");
+//	while (line < 20000)
+//	{
+//		printf("%d哼，\n",line+1);
+//		line++;
+//	}
+//		for (; line < 20000; line++)
+//			printf("%d哼\n",line+1);
+//	printf("啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n");
+//	if (line>= 20000)
+//		printf("太棒了！小田所已经被撅晕过去啦，去撅别人吧！\n");
+//	return 0;
+//}
