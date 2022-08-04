cfb909d96de0ac61333a566a615745b555f3519e
Td-Sm <1225742802@qq.com>
2022-08-05 03:18:35 +0800

    define和指针
    rt

---
+++ 关键字2/关键字2/define.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#define 定义常量和宏
+
+
+//定义标识符常量
+//#define MAX 1000
+//定义宏 - 带参数
+
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+
+//#define MAX(X,Y) (X>Y?X:Y)
+//int main()
+//{
+//	//int a = MAX;
+//	int a = 10;
+//	int b = 20;
+//	//函数
+//	int max = MAX(a, b);
+//	printf("max=%d\n", max);
+//
+//	return 0;
+//}
